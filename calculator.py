48484b7424a775c49aaed0b2272782c8ba270cfe
Aubrey Corcoran <88122787ja9@gmail.com>
2025-11-12 19:56:20 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,3 +1,7 @@
+# https://github.com/CtrlAubDel/lab11-AC-MSG
+# Partner 1: Aubrey Corcoran
+# Partner 2: Mariana Silva Gomez
+
 """
 calculator.py
 - Defines functions used to create a simple calculator
@@ -5,7 +9,33 @@ calculator.py
 One function per operation, in order.
 """
 # First example
+import math
+
 def add(a, b): 
-    pass
+    return a + b
 
+def sub(a, b):
+    return a - b
 
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Can't divide by 0.")
+    else:
+        return a // b
+
+def log(a, b):
+    
+    if a <= 0:
+        raise ValueError("A can not be less than 0.")
+        return
+    if b <= 1:
+        raise ValueError("B must be greater than 1.")
+        return
+    else:
+        return math.log(a, b)
+
+def exp(a, b):
+    return a ** b
