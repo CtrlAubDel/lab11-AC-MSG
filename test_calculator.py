74ead64623441cd068734d8e62b4df29103c111e
Aubrey Corcoran <88122787ja9@gmail.com>
2025-11-12 21:04:55 -0500

    Updated test calculator

--- test_calculator.py
+++ test_calculator.py
@@ -11,11 +11,16 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
+    def test_multiply(self): # 3 assertions
+        self.assertEqual(multiply(2, 3), 6)
+        self.assertEqual(multiply(-4, 5), -20)
+        self.assertEqual(multiply(0, 100), 0)
+        
 
-    # def test_divide(self): # 3 assertions
-    #     fill in code
+    def test_divide(self): # 3 assertions
+        self.assertEqual(divide(10, 2), 5)
+        self.assertEqual(divide(-9, 3), -3)
+        self.assertAlmostEqual(divide(7, 3), 7/3)
     # ##########################
 
     ######## Partner 2
@@ -34,21 +39,30 @@ class TestCalculator(unittest.TestCase):
     # ##########################
     
     ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
+    def test_log_invalid_argument(self): # 1 assertion
     #     # call log function inside, example:
     #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
     #     #     logarithm(0, 5)
-    #     fill in code
+        from calculator import logarithm
+        with self.assertRaises(ValueError):
+            logarithm(0)
 
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
+    def test_hypotenuse(self): # 3 assertions
+        from calculator import hypotenuse
+        self.assertAlmostEqual(hypotenuse(3, 4), 5)
+        self.assertAlmostEqual(hypotenuse(5, 12), 13)
+        self.assertAlmostEqual(hypotenuse(8, 15), 17)
 
-    # def test_sqrt(self): # 3 assertions
+    def test_sqrt(self): # 3 assertions
     #     # Test for invalid argument, example:
     #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
     #     #    square_root(NUM)
     #     # Test basic function
-    #     fill in code
+        from calculator import square_root
+        self.assertEqual(square_root(25), 5)
+        with self.assertRaises(ValueError):
+            square_root(-9)
+        self.assertAlmostEqual(square_root(2), 2**0.5)
     ##########################
 
 # Do not touch this
