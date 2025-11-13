ed8d6b4aaf25a7c1455b8ec6b562debcc65e3704
Mariana Silva <mvsg2311@gmail.com>
2025-11-12 20:00:01 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,11 +1,40 @@
+# https://github.com/mvsg2311-web
+# Partner 1: Aubrey Corcoran
+# Partner 2: Mariana Silva Gomez
+
 """
 calculator.py
 - Defines functions used to create a simple calculator
 
 One function per operation, in order.
 """
-# First example
-def add(a, b): 
-    pass
+import math
 
+def add(a, b):
+    return a + b
 
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Can't divide by 0.")
+    else:
+        return a // b
+
+def log(a, b):
+
+    if a <= 0:
+        raise ValueError("A can not be less than 0.")
+        return
+    if b <= 1:
+        raise ValueError("B must be greater than 1.")
+        return
+    else:
+        return math.log(a, b)
+
+def exp(a, b):
+    return a ** b
